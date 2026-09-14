26de6aeb5965be2d6e20662f80f2a3a49a7ea478
Ori Jimenez <orijimenezv@gmail.com>
2026-09-13 23:21:29 -0300

    Incluye driver pg explícitamente para Vercel (#3)

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
 const { loadConfig } = require('./env');
 const { database: db } = loadConfig();
-const options = { dialect: 'postgres', host: db.host, port: db.port, logging: false,
+const options = { dialect: 'postgres', dialectModule: pg, host: db.host, port: db.port, logging: false,
   pool: { max: db.poolMax, min: 0, acquire: db.connectionTimeoutMillis + 5000, idle: 10000 },
   dialectOptions: { ssl: db.ssl, connectionTimeoutMillis: db.connectionTimeoutMillis, statement_timeout: 10000 } };
 // Pasar campos, nunca URI: evita que pg-connection-string reemplace dialectOptions.ssl.
